d2004620c590742b6eac1608ae37b78e95a7eae5
Eleanor McHugh <eleanor@games-with-brains.com>
2022-03-09 19:56:16 +0000

    2nd example

---
+++ 02.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+
+set :server, %w[ thin mongrel webrick ]
+set :bind, ENV['IP_ADDRESS'] || '0.0.0.0'
+set :port, ENV['PORT'] || 3000
+set :message, ARGV[0] || "Hello World"
+
+get '/' do
+	erb :html_02, locals: { message: settings.message }
+end
