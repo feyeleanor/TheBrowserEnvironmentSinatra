abb515a967f9cf257fc459d9daed3a1a79e15543
Eleanor McHugh <eleanor@games-with-brains.com>
2022-03-10 18:05:49 +0000

    7th example

---
+++ 07.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+
+set :server, %w[ thin mongrel webrick ]
+set :bind, ENV['IP_ADDRESS'] || '0.0.0.0'
+set :port, ENV['PORT'] || 3000
+set :commands, ["A", "B", "C"]
+
+get '/' do
+	erb :html_07, locals: { commands: settings.commands  }
+end
+
+get '/:command' do
+	if settings.commands.include?(params[:command])
+		params[:command]
+	else
+		halt 404
+	end
+end
