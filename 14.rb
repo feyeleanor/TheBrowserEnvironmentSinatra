ca8ec3ffc39fd59efcf2a1c8dfd9369fd9d066b4
Eleanor McHugh <eleanor@games-with-brains.com>
2022-03-12 15:06:57 +0000

    simplified command hash

--- 14.rb
+++ 14.rb
@@ -4,9 +4,9 @@ set :server, %w[ thin mongrel webrick ]
 set :bind, ENV['IP_ADDRESS'] || '0.0.0.0'
 set :port, ENV['PORT'] || 3000
 set :commands, {
-	"A": 300,
-	"B": 700,
-	"C": 500
+	"A" => 300,
+	"B" => 700,
+	"C" => 500
 }
 
 get '/' do
@@ -14,8 +14,8 @@ get '/' do
 end
 
 get '/:command' do
-	if settings.commands.include? params[:command].to_sym
-		settings.commands[params[:command].to_sym].to_s
+	if settings.commands.include? params[:command]
+		settings.commands[params[:command]].to_s
 	else
 		halt 404
 	end
